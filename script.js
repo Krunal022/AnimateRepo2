1e72a5c686b05dc89270fa4a60a21c218eda607d
user.name like: "krunal022 <user.name like :"krunal022>
2024-04-25 16:05:19 +0530

    cdn

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 // alert("Hey you're welcome🫶🏻")
+
+
+Shery.imageEffect("#back",{
+    style:4,
+    debug:true
+});
+
+// Shery.mouseFollower({
+//     skew: true,
+//     ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+//     duration: 1,
+//   });
