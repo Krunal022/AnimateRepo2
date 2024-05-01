ccfb1a2b2dab9788d4c2615508d397c0939ad8c4
user.name like: "krunal022 <user.name like :"krunal022>
2024-05-01 11:06:45 +0530

    gooey effect added

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
 // alert("Hey you're welcome🫶🏻")
 
 
-Shery.imageEffect("#back",{
-    style:4,
-    debug:true
-});
+// Shery.imageEffect("#back",{
+//     style:5,
+//     config:{"a":{"value":1.15,"range":[0,30]},"b":{"value":-0.97,"range":[-1,1]},"zindex":{"value":"-9999999","range":[-9999999,9999999]},"aspect":{"value":1.9417178244337514},"ignoreShapeAspect":{"value":true},"shapePosition":{"value":{"x":0,"y":0}},"shapeScale":{"value":{"x":0.5,"y":0.5}},"shapeEdgeSoftness":{"value":0,"range":[0,0.5]},"shapeRadius":{"value":0,"range":[0,2]},"currentScroll":{"value":0},"scrollLerp":{"value":0.07},"gooey":{"value":true},"infiniteGooey":{"value":true},"growSize":{"value":1,"range":[1,15]},"durationOut":{"value":1,"range":[0.1,5]},"durationIn":{"value":1,"range":[0.1,5]},"displaceAmount":{"value":0.5},"masker":{"value":true},"maskVal":{"value":1.18,"range":[1,5]},"scrollType":{"value":0},"geoVertex":{"range":[1,64],"value":1},"noEffectGooey":{"value":false},"onMouse":{"value":1},"noise_speed":{"value":0.31,"range":[0,10]},"metaball":{"value":0.2,"range":[0,2],"_gsap":{"id":3}},"discard_threshold":{"value":0.46,"range":[0,1]},"antialias_threshold":{"value":0,"range":[0,0.1]},"noise_height":{"value":0.32,"range":[0,2]},"noise_scale":{"value":14.5,"range":[0,100]}},
+//     gooey:true,
+    
+// });
 
-// Shery.mouseFollower({
-//     skew: true,
-//     ease: "cubic-bezier(0.23, 1, 0.320, 1)",
-//     duration: 1,
-//   });
